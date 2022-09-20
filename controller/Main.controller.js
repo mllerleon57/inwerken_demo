bd70e803c9ed5b4a7a569542ec73a1a20af21b67
Leon Möller <lmoeller@inwerken.de>
2022-09-20 10:21:18 +0200

    newest changes

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,15 +1,19 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/demo/basicTemplate/model/formatter"
-], function(Controller, formatter) {
-	"use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/demo/basicTemplate/model/formatter"],
+  function (Controller, formatter) {
+    "use strict";
 
-	return Controller.extend("sap.ui.demo.basicTemplate.controller.Main", {
+    return Controller.extend("sap.ui.demo.basicTemplate.controller.Main", {
+      formatter: formatter,
 
-		formatter: formatter,
+      onInit: function () {},
+      sendMail: function () {
+        const mailAdress = this.byId("mailInput").getValue();
+        sap.m.URLHelper.triggerEmail(mailAdress, "Hier sind Deine Infos der Inwerken AG, schön dass du bei uns warst!", "Infos")
 
-		onInit: function () {
-
-		}
-	});
-});
+		sap.m.MessageToast("Vielen Dank, wir haben Dir eine E-Mail mit allen Informationen gesendet!")
+		this.byId("mailInput").setValue("")
+      },
+    });
+  }
+);
